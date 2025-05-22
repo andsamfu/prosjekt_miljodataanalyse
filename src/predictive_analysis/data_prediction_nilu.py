82a3d40538b28f3bbd7738bae3a2f3232c13dca5
OliverRoddesnes <olivr@stud.ntnu.no>
2025-05-22 11:34:41 +0200

    Endret  datalasting til riktig fil

--- src/predictive_analysis/data_prediction_nilu.py
+++ src/predictive_analysis/data_prediction_nilu.py
@@ -3,21 +3,10 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 
-def load_and_prepare_nilu_data(json_file_path):
-
-    # Last inn data
-    df = pd.read_json(json_file_path)
-    
-    # Konverter 'dateTime' til datetime-format
+def load_and_prepare_nilu_data(file_path):
+    df = pd.read_json(file_path)
     df['dateTime'] = pd.to_datetime(df['dateTime'])
     df = df.sort_values('dateTime')
-    
-    # Lag funksjoner for sesongvariasjoner
-    df['DayOfYear'] = df['dateTime'].dt.dayofyear
-    df['Month'] = df['dateTime'].dt.month
-    df['sin_day'] = np.sin(2 * np.pi * df['DayOfYear'] / 365)
-    df['cos_day'] = np.cos(2 * np.pi * df['DayOfYear'] / 365)
-    
     return df
 
 def split_nilu_data(df, split_ratio=0.75):

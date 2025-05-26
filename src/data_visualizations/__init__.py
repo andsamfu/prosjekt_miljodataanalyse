dbccfdfc148d560233839fea57ee1427d7b0c914
andsamfu <andermhe@stud.ntnu.no>
2025-05-26 16:56:09 +0200

    changed foldername

--- src/data_visualizations/__init__.py
+++ src/data_visualizations/__init__.py
@@ -1,5 +1,5 @@
 from .data_visualizations_dataframes import *
 from .data_visualizations_nilu import *
 from .frost_visualizations import *
-from .frost_vs_nilu import *
+from .frost_vs_nilu_visualizations import *
 from .nilu_visualizations import *
